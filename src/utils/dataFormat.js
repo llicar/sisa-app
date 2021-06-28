9bba388c03e07ab8607c954a864bcee4b5f49505
llicar <lucas.licar.sousa@gmail.com>
2021-06-27 23:06:08 -0300

    corrige erro data invalida

--- src/utils/dataFormat.js
+++ src/utils/dataFormat.js
@@ -5,102 +5,80 @@ class dataFormat {
 
     fullDateBR(props) {
 
-        let date = new Date(props);
-        console.log(date.toLocaleDateString('pt-br',{timeZone: 'UTC'}))
-        return date.toLocaleDateString('pt-br',{timeZone: 'UTC'})
-        /*
-        let date = new Date(`${String(props)} 00:00:00`);
-        console.log(props)
-        console.log('---------------')
-        console.log(date)
-
-
-        if (!isValid(date)) {
-            return 0;
-        } else {
-            let newDate = format(date, 'dd/MM/yyyy');
-            return newDate;
-        }*/
+        if(!props){
+            return 0
+        }
+        else{
+            let date = new Date(props);
+            return date.toLocaleDateString('pt-br',{timeZone: 'UTC'})
+        }
     };
 
     iso(props) {
 
-        const utcStringFormated = props.replace("Z","")
-        let date = new Date(utcStringFormated);
-
-        if (!isValid(date)) {
+        if(!props) {
             return 0
-        } else {
-            let newDate = format(date, 'yyyy-MM-dd',{ locale: ptBR});
-            return newDate;
+        } 
+        else {
+            const utcStringFormated = props.replace("Z","")
+            let date = new Date(utcStringFormated);
+
+            if (!isValid(date)) {
+                return 0
+            } else {
+                let newDate = format(date, 'yyyy-MM-dd',{ locale: ptBR});
+                return newDate;
+            }
         }
+
+        
     };
 
     day(props) {
-
-        const utcStringFormated = props.replace("Z","")
-        let date = new Date(utcStringFormated);
-        return date.getDate()
-       
-        /*
-        let date = new Date(String(props));
-
-        if (!isValid(date)) {
+        
+        if(!props) {
             return 0
-        } else {
-            let newDate = format(date, 'dd');
-            return newDate;
-        }*/
+        } 
+        else{
+            const utcStringFormated = props.replace("Z","")
+            let date = new Date(utcStringFormated);
+            return date.getDate()
+        }
     };
 
     month(props) {
 
-        const utcStringFormated = props.replace("Z","")
-        let date = new Date(utcStringFormated);
-        return date.getMonth()
-
-        /* let date = new Date(String(props));
-
-        if (!isValid(date)) {
+        if(!props){
             return 0
-        } else {
-            let newDate = format(date, 'MM');
-            return newDate;
-        }*/
+        }
+        else{
+            const utcStringFormated = props.replace("Z","")
+            let date = new Date(utcStringFormated);
+            return date.getMonth()
+        }
     };
 
     fullMonth(props) {
-
-        let date = new Date(props);
-        return date.toLocaleDateString('pt-BR',{timeZone: 'UTC',month:'long'})
-
-        /*
-        let date = new Date(String(props));
-
-        if (!isValid(date)) {
+        
+        if(!props) {
             return 0
-        } else {
-            let newDate = format(date, 'MMMM', { locale: ptBR });
-            return newDate;
-        }*/
+        }
+        else{
+            let date = new Date(props);
+            return date.toLocaleDateString('pt-BR',{timeZone: 'UTC',month:'long'})
+        }
     };
 
     year(props) {
 
-        const utcStringFormated = props.replace("Z","")
-        let date = new Date(utcStringFormated);
-        return date.getFullYear()
-
-        /*
-        let date = new Date(String(props));
-
-        if (!isValid(date)) {
-            return 0
-        } else {
-            let newDate = format(date, 'yyyy');
-            return newDate;
-        } */
-
+        if(!props){
+           return 0
+        }
+        else{
+            const utcStringFormated = props.replace("Z","")
+            let date = new Date(utcStringFormated);
+            return date.getFullYear()
+        }
     };
 };
 
