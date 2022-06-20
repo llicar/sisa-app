adf80cd5ba48bd0e65bb7ca5cd3eca3423a113de
llicar <lucas.licar.sousa@gmail.com>
2022-06-19 21:27:56 -0300

    :sparkles: feat: criando services de empresas

--- src/services/empresas.js
+++ src/services/empresas.js
@@ -9,6 +9,14 @@ class EmpresaService {
     async index () {
         return await api.get("/empresas");
     }
+
+    async invEmpresas () {
+        return await api.get("/invEmpresas");
+    }
+
+    async alterarEmpresaPorId (data,id) {
+        return await api.post(`/empresas/${id}`,data);
+    }
     
     async buscarEmpresaPorId (id) {
         return await api.get(`/empresas/${id}`);
