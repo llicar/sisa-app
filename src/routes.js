d337c8fdb85293428bbce67b2a27e2ee9d3da47a
llicar <lucas.licar.sousa@gmail.com>
2022-06-19 21:23:30 -0300

    :sparkles: feat: adicionado rotas de faltas

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ import {useHistory} from 'react-router-dom'
 import Home from './pages/home/index'
 import Login from './pages/Login'
 import autorizado from './utils/auth.js'
+import { ToastContainer} from 'react-toastify';
 
 import CadastrarEmpresa from './pages/CadastrarEmpresa/index'
+import ListarEmpresas from './pages/ListarEmpresas/index'
 import CadastrarJovem from './pages/CadastrarJovem'
 import CadastrarJovem_2 from './pages/CadastrarJovem_2'
 import CadastrarJovem_3 from './pages/CadastrarJovem_3'
@@ -13,6 +15,9 @@ import ListarJovens from './pages/ListarJovens'
 import DetalheJovem from './pages/DetalhesJovem'
 import Admissoes from './pages/Admissoes'
 import VerDadosCadastrais from './pages/VerDadosCadastrais'
+import DetalheEmpresa from './pages/DetalhesEmpresa'
+import CadastrarFalta from './pages/CadastrarFalta'
+import ConsultarFaltas from './pages/ConsultarFaltas'
 
 
 const RotaPrivada = ({component:Component,...rest}) =>{
@@ -37,10 +42,11 @@ const RotaPrivada = ({component:Component,...rest}) =>{
 const Routes = () => {
     return (
             <BrowserRouter>
+            <ToastContainer position={'top-center'} autoClose={2000}/>
                 <Switch>
                     <Route path="/" exact component={Login} ></Route>
                     <RotaPrivada path="/Home" exact component={Home} ></RotaPrivada>
-                    <RotaPrivada path="/CadastrarEmpresa" exact component={CadastrarEmpresa} ></RotaPrivada>
+                    <RotaPrivada path="/ListarEmpresas" exact component={ListarEmpresas} ></RotaPrivada>
                     <RotaPrivada path="/CadastrarJovem" exact component={CadastrarJovem} ></RotaPrivada>
                     <RotaPrivada path="/CadastrarJovem_2/:id" exact component={CadastrarJovem_2} ></RotaPrivada>
                     <RotaPrivada path="/CadastrarJovem_3/:id" exact component={CadastrarJovem_3} ></RotaPrivada>
@@ -48,6 +54,10 @@ const Routes = () => {
                     <RotaPrivada path="/DetalheJovem/:id" exact component={DetalheJovem} ></RotaPrivada>
                     <RotaPrivada path="/Admissoes" exact component={Admissoes} ></RotaPrivada>
                     <RotaPrivada path="/VerDadosCadastrais/:id" exact component={VerDadosCadastrais} ></RotaPrivada>
+                    <RotaPrivada path="/DetalheEmpresa/:id" exact component={DetalheEmpresa} ></RotaPrivada>
+                    <RotaPrivada path="/CadastrarFalta" exact component={CadastrarFalta} ></RotaPrivada>
+                    <RotaPrivada path="/CadastrarEmpresa" exact component={CadastrarEmpresa} ></RotaPrivada>
+                    <RotaPrivada path="/ConsultarFaltas" exact component={ConsultarFaltas} ></RotaPrivada>
                 </Switch>
             </BrowserRouter>
     )
