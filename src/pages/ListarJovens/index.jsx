b9b59320f55cbdca76e63411bad8dc3dfb06782f
llicar <lucas.licar.sousa@gmail.com>
2022-06-19 21:31:11 -0300

    :sparkles: feat: atulizando a tabela para (react-table)

--- src/pages/ListarJovens/index.jsx
+++ src/pages/ListarJovens/index.jsx
@@ -3,53 +3,87 @@ import {useEffect, useState} from 'react'
 
 import {Link} from 'react-router-dom'
 import { CgArrowRightR } from "react-icons/cg";
+import ExportButton from "../../components/ExportButton"
+import {exportarInventario} from "../../utils/exportarInventario"
+import DefaultTable from "../../components/Tables/DefaultTable"
 
 import Header from '../../components/header'
-import DataTable from '../../components/DataTable'
 import JovemService from '../../services/jovens'
 import dataFormat from "../../utils/dataFormat"
-import DotLoader from "react-spinners/DotLoader";
+import SquareLoader from "react-spinners/SquareLoader";
+
+
 import style from './style.css'
+
+import {DateRangeColumnFilter} from '../../components/Tables/filters/RangeFilter'
+import Filter from '../../components/Tables/filters/filter'
 
 
 const ListarJovens = () => {
-    
+
+     const COLUMNS = [
+       
+        {
+          Header: 'NOME',
+          accessor: 'nome',
+          Filter
+        },
+        {
+          Header: 'EMPRESA',
+          accessor: 'empresa',
+          Filter
+        },
+        {
+          Header: 'ADMISSÃO',
+          accessor: 'admissao',
+          Filter:DateRangeColumnFilter,
+          filter: "dateBetween",
+        },
+        {
+          accessor: 'detalhes',
+          Filter,
+          disableFilters: true
+        },
+      ];
+
     const [data,setData] = useState([]);
     const [showLoader,setShowLoader] = useState(true);
 
+    
     useEffect(() => {
+
         JovemService.index()
         .then( response => {
-            setShowLoader(false);
             const newData = response.data.map( (response) => {
                 const newObg = 
                     {
-                        ID: response.id_jovem,
-                        Nome: response.nome,
-                        Admissao: dataFormat.fullDateBR(response.admissao),
-                        Empresa: response.nome_fantasia,
-                        Detalhes: <Link to={`/DetalheJovem/${response.id_jovem}`}><CgArrowRightR  className="linkArrow"/></Link>
+                        nome: response.nome,
+                        admissao: dataFormat.fullDateBR(response.admissao) ,
+                        empresa: response.nome_fantasia,
+                        detalhes: <Link to={`/DetalheJovem/${response.id_jovem}`}><CgArrowRightR  className="linkArrow"/></Link>
                        
                     }
-                
                 return newObg;
             })
             setData(newData);
-        })
-    },[])  
+            setShowLoader(false);
+        })},[])  
+
 
     return (
         <body>
             <Header/>
+            <div className="container_itens">
+            </div>
           
                 {
                     showLoader?
                         <div className="loader">
-                        <DotLoader color={'#1EC3BA'}/> 
+                        <SquareLoader color={'#1EC3BA'}/> 
                         </div>
                         
                         :
-                        <DataTable data={data}></DataTable>
+                        <DefaultTable COLUMNS={COLUMNS} DATA={data}></DefaultTable>
                 }
                 
         </body>
