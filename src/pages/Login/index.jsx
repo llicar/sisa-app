722c1d4b627fc61391e80b541ed01a97e87beeb1
llicar <lucas.licar.sousa@gmail.com>
2022-07-03 23:13:21 -0300

    feact: implementando login como usuarios

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,20 @@
 import {useState} from 'react'
 import {useForm} from 'react-hook-form'
 import {useHistory} from 'react-router-dom'
+import jwt from "jsonwebtoken";
 
 import {Form} from '../../components/form'
 import {InputText} from '../../components/input'
 import {InputContainer} from '../../components/inputContainer'
 import {ContainerLogin,HeaderLogin} from './style.js'
 import DotLoader from "react-spinners/DotLoader";
+import {toast } from 'react-toastify';
+import ServiceUsuario from '../../services/usuario'
 
-import ServiceLogin from '../../services/login'
+import logo from '../../assets/images/logo_sisa.svg'
 
-import logo from '../../assets/images/logo_aedha.svg'
+const loginErrNotif = () => toast('Usuário ou senha inválido',{type:'error',theme: "colored"})
+const loginNotfi = (userName) => toast(`seja bem-vindo(a),${userName} 🎉`,{type:'default',theme: "colored"})
 
 const Login = () => {
 
@@ -22,23 +26,28 @@ const Login = () => {
     async function submit(data){
         try{
             setLoading(true)
-            const response = await  ServiceLogin.login(data);
-            localStorage.setItem('@sisa-app/token',response.data.token);
+            const response = await  ServiceUsuario.login(data);
+            const permissions = await ServiceUsuario.permissoes(response.data.user_id)
+            localStorage.setItem('@sisa-app/token',response.data.token,);
+            localStorage.setItem('@sisa-app/user_id',response.data.user_id);
+            localStorage.setItem('@sisa-app/user_permissions',permissions.data);
             history.push('/Home')
+            const firstName = response.data.user_name.split(' ')
+            loginNotfi(firstName[0])
         }catch(err){
-            alert('Usuario ou senha incorreto, tente novamente')
+            loginErrNotif()
             setLoading(false)
         }
     }
     return (
         <body>
         <HeaderLogin>
-            <img src={logo} alt='logo aedha'/>
+            <img src={logo} style={{width:'100px'}} alt='logo aedha'/>
             <h1>SISA</h1>
         </HeaderLogin>
 
         <ContainerLogin>
-            <h1>Bem-vindo</h1>
+            <h1>Olá</h1>
 
             <Form style={{width: '100%'}} onSubmit={handleSubmit(submit)}>
             <InputContainer style={{width: '80%'}}>
